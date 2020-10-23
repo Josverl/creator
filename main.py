5615164d0b97ba7e88aaa75d7725e14e867fb517
Jos Verlinde <josverl@microsoft.com>
2020-10-23 16:37:59 +0200

    dummy stubber

---
+++ main.py
@@ -0,0 +1,9 @@
+"Just create new dummy stubfile"
+
+import tempfile
+
+tf = tempfile.NamedTemporaryFile(delete=False,prefix='stub_',dir='./stubs',suffix='.py', mode="w+")
+print(tf.name)
+tf.write("# Just a stubfile")
+tf.close()
+
